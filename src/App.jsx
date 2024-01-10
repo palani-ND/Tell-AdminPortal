606c3b63063a8039ea2ba10ef7a076f9ea0e6413
suresh6602 <sureshkumar@navadhiti.com>
2024-01-10 15:42:19 +0530

    update the folder structure added a husky,eslint,prettier

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { theme } from './theme';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const App = () => {
-    const queryClient = new QueryClient();
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-                <h1>Hello world</h1>
-            </ThemeProvider>
-        </QueryClientProvider>
-    );
+	const queryClient = new QueryClient();
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={theme}>
+				<h1>Hello world</h1>
+			</ThemeProvider>
+		</QueryClientProvider>
+	);
 };
 export default App;
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,8 +1,1 @@
 // layout folder for using page layout
-import React from 'react';
-
-function index() {
-    return <div></div>;
-}
-
-export default index;
