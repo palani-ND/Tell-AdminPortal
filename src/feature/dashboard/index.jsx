b2c5331d472a8c7403e87720c51ec778e6b2b435
suresh6602 <sureshkumar@navadhiti.com>
2024-02-02 11:02:54 +0530

    add a paper component of the csv file

--- src/feature/dashboard/index.jsx
+++ src/feature/dashboard/index.jsx
@@ -1,7 +1,26 @@
+import { Box, Paper } from '@mui/material';
 import React from 'react';
 
 const Dashboard = () => {
-	return <>Dashboard component</>;
+	return (
+		<>
+			<Box>
+				<Paper
+					sx={{
+						display: 'flex',
+						alignItems:'center',
+						justifyContent: 'center',
+						height: '150px',
+						borderRadius: '10px',
+						fontSize:'18px'
+					}}
+					elevation={5}
+				>
+					Upload your csv file 
+				</Paper>
+			</Box>
+		</>
+	);
 };
 
 export default Dashboard;
